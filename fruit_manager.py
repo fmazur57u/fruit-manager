ddfc270749b319dd33500efbc5600e2f9231987a
fmazur57u <florianmazur2@gmail.com>
2025-11-21 12:33:07 +0100

    Ajout de la fonctionnalité tresorerie

--- fruit_manager.py
+++ fruit_manager.py
@@ -1,15 +1,30 @@
 import json
 
 
-def ouvrir_inventaire(path="inventaire.json"):
+def ouvrir_inventaire(path="data/inventaire.json"):
     with open(path, "r", encoding="utf-8") as fichier:
         inventaire = json.load(fichier)
     return inventaire
 
 
-def ecrire_inventaire(inventaire, path="inventaire.json"):
+def ecrire_inventaire(inventaire, path="data/inventaire.json"):
     with open(path, "w", encoding="utf-8") as fichier:
         json.dump(inventaire, fichier, ensure_ascii=False, indent=4)
+
+
+def ouvrir_tresorerie(path="data/tresorerie.txt"):
+    with open(path, "r", encoding="utf-8") as fichier:
+        tresorerie = json.load(fichier)
+    return tresorerie
+
+
+def ecrire_tresorerie(tresorerie, path="data/tresorerie.txt"):
+    with open(path, "w", encoding="utf-8") as fichier:
+        json.dump(tresorerie, fichier, ensure_ascii=False, indent=4)
+
+
+def afficher_tresorerie(tresorerie):
+    print(f"\n Trésorerie actuelle : {tresorerie:.2f} $")
 
 
 def afficher_inventaires(inventaire):
@@ -23,16 +38,24 @@ def recolter(inventaire, fruit, quantite):
     print(f"\n Récolté {quantite} {fruit} supplémentaires !")
 
 
-def vendre(inventaire, fruit, quantite):
+def vendre(inventaire, fruit, quantite, tresorerie):
     if inventaire.get(fruit, 0) >= quantite:
         inventaire[fruit] -= quantite
+        tresorerie += 1 * quantite
         print(f"\n Vendu {quantite} {fruit} !")
+        return (inventaire, tresorerie)
     else:
         print(f"\n Pas assez de {fruit} pour vendre {quantite} unités.")
 
 
 if __name__ == "__main__":
+    inventaire = ouvrir_inventaire()
+    tresorerie = ouvrir_tresorerie()
+    afficher_tresorerie(tresorerie)
     afficher_inventaires(inventaire)
+
     recolter(inventaire, "bananes", 10)
-    vendre(inventaire, "bananes", 5)
-    afficher_inventaires(inventaire)
+    inventaire, tresorerie = vendre(inventaire, "bananes", 5, tresorerie)
+
+    ecrire_inventaire(inventaire)
+    ecrire_tresorerie(tresorerie)
